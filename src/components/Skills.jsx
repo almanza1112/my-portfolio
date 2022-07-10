9b673dc953eeaf60d7a19383e61284770148ca08
almanza1112 <almanza1112@gmail.com>
2022-07-10 03:14:48 -0400

    Skills section complete

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css3.png'
+import JavaScript from '../assets/javascript.png'
+import Node from '../assets/node-js.png'
+import ReactImg from '../assets/react.png'
+import MongoDB from '../assets/mongodb.png'
+import Flutter from '../assets/flutter.png'
+import Firebase from '../assets/firebase.png'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt='HTML logo'></img>
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt='Css logo'></img>
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt='JavaScript logo'></img>
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt='React logo'></img>
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Node} alt='Node JS logo'></img>
+                    <p className='my-4'>NODE.JS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={MongoDB} alt='MongoDB logo'></img>
+                    <p className='my-4'>MONGODB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Flutter} alt='Flutter logo'></img>
+                    <p className='my-4'>FLUTTER</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Firebase} alt='Firebase logo'></img>
+                    <p className='my-4'>FIREBASE</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
