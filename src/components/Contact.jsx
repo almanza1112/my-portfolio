b93a6f07b9cf5666b449b91a534ed40b332e2117
almanza1112 <almanza1112@gmail.com>
2022-07-10 04:17:19 -0400

    Conatct section init

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full h-screen text-gray-300 bg-[#0a192f]'>
+        
+    </div>
+  )
+}
+
+export default Contact
