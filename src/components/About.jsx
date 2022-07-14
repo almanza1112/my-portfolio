1c6c067a8095e67dc2b061786c48fe634d1c868f
almanza1112 <almanza1112@gmail.com>
2022-07-14 10:51:58 -0400

    responsive text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 </div>
             </div>
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-                <div className='sm:text-right text-4xl font-bold '>
+                <div className='sm:text-right text-xl sm:text-4xl font-bold '>
                     <p>A developer you can trust</p>
                 </div>
                 <div>
